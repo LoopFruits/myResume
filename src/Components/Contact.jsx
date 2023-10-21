671f31b7004de254f8b51affba7a1a5c841c2677
Kenneth Jordan <kensworldc@gmail.com>
2023-10-21 08:29:30 -0400

    working tailwind

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,10 +1,10 @@
-const Contact  = () => {
-
-    return(
-    <>
-    </>
-    )
-
+const  Contact = () => {
+    return (
+    <div className="flex justify-between items-center w-full h-20 px-4 text-lg text-white bg-blue fixed">
+        Kenneth Jordan
+        
+    </div>
+    );
 }
-
+    
 export default Contact ;
